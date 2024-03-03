cc667d65d79cd3e3343c4ce4177c12e6a1d0c056
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-03 14:02:41 +0500

    Design the background of the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Background from '../components/Background';
 
 function App() {
+  let heroData =[
+    {text1:"Drive into", text2: "What you love"},
+    {text1:"Indulge", text2: "Your Passion"},
+    {text1:"Give in to", text2: " your  passions"},
+  ]
+  const [heroCount, setheroCount] = useState(2);
+  const [playStatus, setplayStatus] = useState(false);
   return (
-    <div className="App">
+    <div>
      
+     <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   );
 }
